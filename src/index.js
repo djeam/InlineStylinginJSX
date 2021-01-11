2af49e3e66114daff46fa3728a542db69854ae1d
djeam <60947080+djeam@users.noreply.github.com>
2021-01-11 06:31:58 -0500

    Practice Inline Styling with JSX Inserting CSS code into a React app through inline styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const customStyle = {
+  color: "red",
+  fontSize: "40px",
+  border: "5px solid blue",
+  borderRadius: "10px",
+  boxShadow: "10px 10px 8px #888888"
+};
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
